63ac90e4f93d6b8322a3aba8e0a0b20f401cd362
Hritik <hritikm46@gmail.com>
2020-09-03 11:43:42 +0530

    models created for smtp config in bodhi keyspace

---
+++ src/main/java/com/infosys/model/cassandra/SmtpConfigPrimaryKeyModel.java
@@ -0,0 +1,26 @@
+package com.infosys.model.cassandra;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+
+import java.io.Serializable;
+
+@PrimaryKeyClass
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SmtpConfigPrimaryKeyModel implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@PrimaryKeyColumn(name = "root_org", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+	private String rootOrg;
+
+	@PrimaryKeyColumn(name = "org", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+	private String org;
+
+}
